9e0fbf9b39667f64e79851d33f2a5d9d365d1c53
Ashish Gupta <ashishgupta13@gmail.com>
2012-01-11 16:58:30 -0800

    First checkin with initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabricez::Application.config.secret_token = 'e132f2fe12e523fdd64b9a120ede4c7ade6c22d20f5c0eacfc0d3977b6ae4de3994541e8fab33d6ea016f902f2a9f700c490cee9f661245ab9505dc13b95abf4'
